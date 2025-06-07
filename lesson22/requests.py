b0501847f5d68864a3ff6c9d87427ffabfce33b5
Student <rijanazemi4@gmail.com>
2025-06-07 03:32:41 -0700

    k

---
+++ lesson22/requests.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+@app.post("/create_person")
+async def create_person(person: Person):
+    return{"message:" f"Person {person.name} created with age {person.age}"}
